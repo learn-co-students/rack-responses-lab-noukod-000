42d03fd375d2953beaf111633e8643f9b46d261c
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-22 18:06:42 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+
+  def call(env)
+    #resp = Rack::Response.new
+    #resp.write "Hello, World"
+    #resp.finish
+    
+    resp = Rack::Response.new
+ 
+    time = Time.now.hour
+
+    if time <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
